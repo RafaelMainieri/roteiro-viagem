87273abc1427a5d3e11fee0e4306ab436c1720f0
Rafael Mainieri <rafasolmainieri@gmail.com>
2024-07-11 21:36:52 -0300

    aula2-js

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-// objeto {}
+// formatador de data
+const formatador = (data) => {
+	return {
+		dia: {
+			numerico: dayjs(data).format('DD'),
+			semana: {
+				curto: dayjs(data).format('ddd'),
+				longo: dayjs(data).format('dddd'),
+			},
+		},
+		mes: dayjs(data).format('MMMM'),
+		hora: dayjs(data).format('HH:mm'),
+	};
+};
+
+formatador(new Date('2024-04-01'));
+
+// atividades
 const atividade = {
 	nome: 'Almoço',
 	data: new Date('2024-07-08 10:00'),
 	finalizada: true,
 };
 
-const atividades = [
+let atividades = [
 	atividade,
 	{
 		nome: 'Academia em grupo',
@@ -19,26 +36,124 @@ const atividades = [
 	},
 ];
 
-// arroy function → criando os itens
+// criação e formatação dos itens
 const criarItemDeAtividade = (atividade) => {
-	let input = '<input type="checkbox" ';
+	let input = `
+	<input
+		onchange="concluirAtividade(event)" 
+		value="${atividade.data}"
+		type="checkbox" `;
 
+	// verificação do check
 	if (atividade.finalizada) {
 		input += 'checked';
 	}
 	input += '>';
 
+	const formatar = formatador(atividade.data);
+
 	return `
     <div>
         ${input}
         <span>${atividade.nome}</span>
-        <time>${atividade.data}</time>
+        <time>
+			${formatar.dia.semana.longo}, dia
+		 	${formatar.dia.numerico} de
+			${formatar.mes} às
+		 	${formatar.hora}h
+		</time>
       </div>`;
 };
 
-const section = document.querySelector('section');
+// atualização dos itens (mostrar na página)
+const atualizarListaDeAtividades = () => {
+	const section = document.querySelector('section');
+	section.innerHTML = '';
 
-for (let atividade of atividades) {
-	// muda o conteúdo que está dentro da tag section
-	section.innerHTML += criarItemDeAtividade(atividade);
-}
+	if (atividades.length == 0) {
+		section.innerHTML = `<p>Nenhuma atividade cadastrada.</p>`;
+		return;
+	}
+
+	for (let atividade of atividades) {
+		// muda o conteúdo que está dentro da tag section
+		section.innerHTML += criarItemDeAtividade(atividade);
+	}
+};
+atualizarListaDeAtividades();
+
+const salvarAtividade = (event) => {
+	event.preventDefault();
+	const dadosDoFormulario = new FormData(event.target);
+
+	const nome = dadosDoFormulario.get('atividade');
+	const dia = dadosDoFormulario.get('dia');
+	const hora = dadosDoFormulario.get('hora');
+	const data = `${dia} ${hora}`;
+
+	const novaAtividade = {
+		nome,
+		data,
+		finalizada: false,
+	};
+
+	const atividadeExiste = atividades.find((atividade) => {
+		return atividade.data == novaAtividade.data;
+	});
+
+	if (atividadeExiste) {
+		return alert('Dia/Hora não disponível');
+	}
+
+	atividades = [novaAtividade, ...atividades]; // esses ... servem pra colocar as atividades antigas na lista
+	atualizarListaDeAtividades();
+};
+
+// opções de dias
+const criarDiasSelecao = () => {
+	const dias = ['2024-02-28', '2024-02-29', '2024-03-01', '2024-03-02', '2024-03-03'];
+
+	let diasSelecao = '';
+
+	for (let dia of dias) {
+		const formatar = formatador(dia);
+		const diaFormatado = `${formatar.dia.numerico} de ${formatar.mes}`;
+
+		diasSelecao += `
+		<option value="${dia}">${diaFormatado}</option>`;
+	}
+
+	// procura a tag select que tem como name=dia
+	document.querySelector('select[name="dia"]').innerHTML = diasSelecao;
+};
+criarDiasSelecao();
+
+// opções de horário
+const criarHorasSelecao = () => {
+	let horasDisponiveis = '';
+
+	for (let i = 0; i < 24; i++) {
+		horasDisponiveis += `<option value="${i}:00">${i}:00</option>`;
+		horasDisponiveis += `<option value="${i}:30">${i}:30</option>`;
+	}
+
+	document.querySelector('select[name="hora"]').innerHTML = horasDisponiveis;
+};
+criarHorasSelecao();
+
+const concluirAtividade = (event) => {
+	const input = event.target;
+	const dataDesteInput = input.value;
+
+	// uma especie de verificação dentro da função
+	const atividade = atividades.find((atividade) => {
+		return atividade.data == dataDesteInput;
+	});
+
+	if (!atividade) {
+		return;
+	}
+
+	// inverte a info (se era true vira false, se era false vira true)
+	atividade.finalizada = !atividade.finalizada;
+};
